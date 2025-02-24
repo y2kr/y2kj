fb017f64dd5065e4f57bdae95a8fe776aed77dae
y2kr <joemotoloto@gmail.com>
2025-02-24 15:04:36 +0000

    update vite config2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-export default {
-  base: '/y2krdemo/',
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  base: '/y2krdemo/', // Set the base path to match your GitHub Pages subdirectory
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        photos: resolve(__dirname, 'photos.html'),
+      },
+    },
   },
-};
+});
